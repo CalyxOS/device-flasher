a33be1d44bdb68eccb133b3d9fa9e177095f1915
Dan Vittegleo <dan@vittegleo.com>
2020-10-13 00:34:09 -0700

    add platformtools, adb, fastboot
    Change-Id: I40dd1b82dd2097a29c2931c6629b930b8b5365b4

---
+++ internal/platformtools/adb/adb.go
@@ -0,0 +1,76 @@
+package adb
+
+import (
+	"gitlab.com/calyxos/device-flasher/internal/platformtools"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	adbExecutable = "adb"
+)
+
+type Tool struct {
+	executable string
+}
+
+func New(path platformtools.PlatformToolsPath, osName string) (*Tool, error) {
+	executable := filepath.Join(string(path), adbExecutable)
+	if osName == "windows" {
+		executable = executable + ".exe"
+	}
+	if _, err := os.Stat(executable); os.IsNotExist(err) {
+		return nil, err
+	}
+	return &Tool{
+		executable: executable,
+	}, nil
+}
+
+func (t *Tool) GetDeviceIds() ([]string, error) {
+	resp, err := t.command([]string{"devices"})
+	if err != nil {
+		return nil, err
+	}
+	devices := strings.Split(string(resp), "\n")
+	devices = devices[1 : len(devices)-2]
+	for i, device := range devices {
+		devices[i] = strings.Split(device, "\t")[0]
+	}
+	return devices, nil
+}
+
+func (t *Tool) GetDeviceCodename(deviceId string) (string, error) {
+	return  t.getProp("ro.product.device", deviceId)
+}
+
+func (t *Tool) RebootIntoBootloader(deviceId string) error {
+	_, err := t.command([]string{"-s", deviceId, "reboot", "bootloader"})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Tool) KillServer() error {
+	_, err := t.command([]string{"kill-server"})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Tool) command(args []string) ([]byte, error) {
+	cmd := exec.Command(t.executable, args...)
+	return cmd.CombinedOutput()
+}
+
+func (t *Tool) getProp(prop, deviceId string) (string, error) {
+	resp, err := t.command([]string{"-s", deviceId, "shell", "getprop", prop})
+	if err != nil {
+		return "", err
+	}
+	return strings.Trim(string(resp), "[]\n\r"), nil
+}
---
+++ internal/platformtools/platformtools.go
@@ -0,0 +1,168 @@
+package platformtools
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"github.com/mholt/archiver/v3"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"os"
+)
+
+const (
+	DefaultBaseURI = "https://dl.google.com/android/repository"
+	PlatformToolsFilenameTemplate = "platform-tools-%v-%v.zip"
+)
+
+const (
+	LinuxSha256   = "f7306a7c66d8149c4430aff270d6ed644c720ea29ef799dc613d3dc537485c6e"
+	DarwinSha256  = "ab9dbab873fff677deb2cfd95ea60b9295ebd53b58ec8533e9e1110b2451e540"
+	WindowsSha256 = "265dd7b55f58dff1a5ad5073a92f4a5308bd070b72bd8b0d604674add6db8a41"
+)
+
+type PlatformToolsPath string
+
+type Config struct {
+	BaseURI string
+	HttpClient *http.Client
+	HostOS string
+	ToolsVersion string
+	DestinationDirectory string
+	Logger *logrus.Logger
+}
+
+type PlatformTools struct {
+	httpClient       *http.Client
+	os               string
+	downloadURI      string
+	sha256           string
+	workingDirectory string
+	zipFile          string
+	path             string
+	logger           *logrus.Logger
+}
+
+func New(config *Config) (*PlatformTools, error) {
+	platformToolsFilename := fmt.Sprintf(PlatformToolsFilenameTemplate, config.ToolsVersion, config.HostOS)
+	downloadURI := fmt.Sprintf("%v/%v", DefaultBaseURI, platformToolsFilename)
+	workingDirectory := config.DestinationDirectory
+	zipFile := fmt.Sprintf("%v/%v", workingDirectory, "platform-tools.zip")
+	path := fmt.Sprintf("%v/platform-tools", workingDirectory)
+
+	var sha256 string
+	switch config.HostOS {
+	case "linux":
+		sha256 = LinuxSha256
+	case "darwin":
+		sha256 = DarwinSha256
+	case "windows":
+		sha256 = WindowsSha256
+	}
+
+	platformTools := &PlatformTools{
+		path: path,
+		httpClient: config.HttpClient,
+		downloadURI: downloadURI,
+		sha256: sha256,
+		workingDirectory: workingDirectory,
+		zipFile: zipFile,
+		logger: config.Logger,
+	}
+
+	err := platformTools.initialize()
+	if err != nil {
+		return nil, err
+	}
+
+	return platformTools, nil
+}
+
+func (p *PlatformTools) initialize() error {
+	logger := p.logger.WithFields(logrus.Fields{
+		"downloadURI": p.downloadURI,
+		"sha256": p.sha256,
+		"zipFile": p.zipFile,
+	})
+
+	logger.Debug("starting tools download")
+	err := p.download()
+	if err != nil {
+		return err
+	}
+
+	logger.Debug("starting tools extract")
+	err = p.extract()
+	if err != nil {
+		return err
+	}
+
+	// TODO: add back verify
+	//logger.Debug("starting tools verify")
+	//err = p.verify()
+	//if err != nil {
+	//	return err
+	//}
+
+	return nil
+}
+
+func (p *PlatformTools) Path() PlatformToolsPath {
+	return PlatformToolsPath(p.path)
+}
+
+func (p *PlatformTools) download() error {
+	p.logger.Debugf("making directory %v", p.workingDirectory)
+	_ = os.Mkdir(p.workingDirectory, os.ModePerm)
+
+	p.logger.Debugf("creating file %v", p.zipFile)
+	out, err := os.Create(p.zipFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	p.logger.Debugf("downloading %v", p.downloadURI)
+	resp, err := p.httpClient.Get(p.downloadURI)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad download status from %v: %v", p.zipFile, resp.Status)
+	}
+
+	p.logger.Debugf("copying data to %v", p.zipFile)
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *PlatformTools) extract() error {
+	p.logger.Debugf("unzipping %v to directory %v", p.zipFile, p.workingDirectory)
+	return archiver.Unarchive(p.zipFile, p.workingDirectory)
+}
+
+func (p *PlatformTools) verify() error {
+	p.logger.Debugf("verifying sha256 for %v = %v", p.zipFile, p.sha256)
+	f, err := os.Open(p.zipFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return err
+	}
+	sum := hex.EncodeToString(h.Sum(nil))
+	if p.sha256 != sum {
+		return fmt.Errorf("expected sha256 to be %v but got %v", p.sha256, sum)
+	}
+	return nil
+}
