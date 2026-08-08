36c1709475d9bd183602a04de3f9a41aca7fad82
Michael Bestas <mkbestas@gmail.com>
2026-08-08 03:16:47 +0300

    Add build script and update README
    Change-Id: I604c20af6967638e950d6bef228ec654217638e2

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -eu
+
+image=${IMAGE:-golang:1.26.5}
+
+exec docker run --rm \
+	-v "$PWD":/src \
+	-w /src \
+	--user "$(id -u):$(id -g)" \
+	-e GOCACHE=/tmp/gocache \
+	-e GOPATH=/tmp/gopath \
+	"$image" \
+	sh -c 'make'
